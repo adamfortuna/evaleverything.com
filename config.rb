a5a679215e699853b83f037aa5313e62ffac31b7
Adam Fortuna <adam@fortuna.name>
2013-09-02 18:23:54 -0400

    Initial Setup

--- config.rb
+++ config.rb
@@ -4,11 +4,11 @@ require 'sass-globbing'
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
+http_path = "/evaleverything/"
+http_images_path = "/evaleverything/images"
 http_generated_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_fonts_path = "/evaleverything/fonts"
+css_dir = "public/evaleverything/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
